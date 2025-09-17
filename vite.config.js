8392daad6af52117e63b9710a175a0c571a769a2
rosedvel <ros19ed@gmail.com>
2025-09-16 23:48:02 -0500

    fix: eliminar vercel.json y corregir ruta del script

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
-  root: '.',
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      }
-    }
-  },
   plugins: [react()],
   server: {
     port: 3000,
