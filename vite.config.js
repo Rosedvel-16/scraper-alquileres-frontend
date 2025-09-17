db2b8338818e3fb0f2480f01d5fa317eb61565aa
rosedvel <ros19ed@gmail.com>
2025-09-16 23:40:22 -0500

    Actualizacion

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
+  root: '.',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
+  },
   plugins: [react()],
   server: {
     port: 3000,
